f90b52804f77ad1fcce9eeb68270c03d33169235
saeed.at <saeed.alijani@aut.ac.ir>
2022-02-27 18:31:46 +0330

    Add files via upload

--- pwm.c
+++ pwm.c
@@ -2,24 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-conts int PWM_PIN = 25;
+conts int PWM_PIN = 13;//physical por number 
 
 int main(){
-    int duty_cycle;
     wiringPiSetupGpio();
-    pinMode(1, PWM_OUTPUT);
+    pinMode(PWM_PIN, PWM_OUTPUT);
     pwmSetMode(PWM_MODE_MS);
 
     pwmSetClock(187);
     pwmSetRange(1024);
 
-    system("clear");
     while(1){
-        print("please insert duty cycle:");
-        scanf("%d". &duty_cycle);
-        system("clear");
-        printf("\n duty = %d of \'1023\'\n", duty_cycle);
-        pwmWrite(PWM_PIN, duty_cycle);
-
+        for (int i = 0; i < 101; i+=5){
+            pwmWrite(PWM_PIN, i);
+            delay(500);
+        }
     }
 }
