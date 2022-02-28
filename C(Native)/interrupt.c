6c5ef6c949aa667d4308567d7e91bdf549a775ee
saeed.at <saeed.alijani@aut.ac.ir>
2022-02-28 16:56:51 +0330

    Update interrupt.c

--- C(Native)/interrupt.c
+++ C(Native)/interrupt.c
@@ -1,7 +1,7 @@
 #include <wiringPi.h>
 #include <stdio.h>
-int LED = 26;			/* GPIO26 as per wiringPi, GPIO12 as per BCM, pin no.32 */
-int switch_input = 21;          /* GPIO21 as per WiringPi, GPIO5 as per BCM, pin no.29 */
+int LED = 25;			/* GPIO26 as per wiringPi, GPIO12 as per BCM, pin no.32 */
+int switch_input = 22;          /* GPIO21 as per WiringPi, GPIO5 as per BCM, pin no.29 */
 int main(){
 	wiringPiSetup();	/* initialize wiringPi setup */
 	pinMode(LED,OUTPUT);	/* set GPIO as output */
